1271976de5983b3781885001d1aed903813898df
Chris Hoffman <chris.c.hoffman@gmail.com>
2011-03-01 21:24:56 -0500

    creating rails app for this stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoffmancGithubCom::Application.config.secret_token = '37bfadb6cda6dfa5d02de33302427c09a16360328c18c64562ab1a26b32b4ba7c34ecb25abee4e47bebe162d8cd1362e5f2d5a53dd84522522875bb593963d7e'
